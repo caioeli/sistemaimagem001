5cfc24331f362552cc1cd46bbdff3887525c01c4
caioeli <caio.eli0234@gmail.com>
2022-04-18 09:53:08 -0300

    Adicionar arquivos de projeto.

---
+++ sistemaimagem001/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace sistemaimagem001
+{
+    partial class IMAGEM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelimg = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn = new System.Windows.Forms.Button();
+            this.panelimg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelimg
+            // 
+            this.panelimg.Controls.Add(this.pictureBox);
+            this.panelimg.Location = new System.Drawing.Point(41, 29);
+            this.panelimg.Name = "panelimg";
+            this.panelimg.Size = new System.Drawing.Size(598, 336);
+            this.panelimg.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(598, 336);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btn
+            // 
+            this.btn.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btn.Location = new System.Drawing.Point(290, 390);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(133, 48);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Buscar";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // IMAGEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(689, 450);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.panelimg);
+            this.Name = "IMAGEM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.IMAGEM_Load);
+            this.panelimg.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelimg;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btn;
+    }
+}
---
+++ sistemaimagem001/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemaimagem001
+{
+    public partial class IMAGEM : Form
+    {
+        public IMAGEM()
+        {
+            InitializeComponent();
+        }
+
+        private void IMAGEM_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Arquivos mbp | *.bmp|Arquivos jpg | *.jpg";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.ImageLocation = openFileDialog.FileName;
+            }
+        }
+    }
+}
